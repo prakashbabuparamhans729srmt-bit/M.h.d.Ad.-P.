a5580598797a7451858e4c65f6145cc5ae7164bc
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-20 06:57:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9341424071-e15fd",
+  "appId": "1:748755550030:web:23fa96f6f7a8ecf78c281f",
+  "apiKey": "AIzaSyAlP3ZVJzI1GPQIDJKqgQy2eu-eeNlELWo",
+  "authDomain": "studio-9341424071-e15fd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "748755550030"
+};
