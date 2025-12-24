d397c516501e3bbc12681b0ec4843a4665a7609b
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-12-24 08:09:34 +0000

    dashboard me tablet ,mobile me side bar open karne ke liye icon set kare

--- src/components/layout/portal-layout.tsx
+++ src/components/layout/portal-layout.tsx
@@ -11,6 +11,28 @@ import {
 import { Logo } from '@/components/shared/logo';
 import { UserNav } from '@/components/shared/user-nav';
 import { FloatingSupportWidget } from '../shared/floating-support-widget';
+import { Button } from '../ui/button';
+import { PanelLeft } from 'lucide-react';
+import { useSidebar } from '../ui/sidebar';
+
+function MobileHeader() {
+    const { toggleSidebar } = useSidebar();
+    return (
+         <header className="flex md:hidden h-16 items-center gap-4 border-b bg-background px-4">
+            <Button
+                variant="ghost"
+                size="icon"
+                onClick={toggleSidebar}
+            >
+                <PanelLeft className="h-6 w-6" />
+                <span className="sr-only">Toggle Sidebar</span>
+            </Button>
+            <div className="flex-1 text-right">
+              <UserNav />
+            </div>
+          </header>
+    )
+}
 
 type PortalLayoutProps = {
   sidebarContent: ReactNode;
@@ -30,12 +52,14 @@ export function PortalLayout({ sidebarContent, children }: PortalLayoutProps) {
           </SidebarFooter>
         </Sidebar>
         <SidebarInset>
-          <header className="flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-            <SidebarTrigger className="md:flex hidden" />
+          <header className="hidden md:flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <SidebarTrigger />
             <div className="flex-1 text-right">
               <UserNav />
             </div>
           </header>
+          {/* Mobile header */}
+          <MobileHeader />
           <main className="flex-1 p-4 md:p-6">{children}</main>
           <FloatingSupportWidget />
         </SidebarInset>
